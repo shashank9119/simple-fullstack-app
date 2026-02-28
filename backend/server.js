a129e35fec8a03acef3724aeb8e860cb5598b3fe
shashank9119 <118582678+shashank9119@users.noreply.github.com>
2026-02-28 23:05:54 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from Backend 🚀" });
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
